ceff34e488e9ca02fde7bd8cc74866ebdc8f5fde
João Mariz <joao@joaomariz.eu>
2022-05-30 19:36:34 +0100

    Add project files.

---
+++ RS6_OOP_P1_2022-05_JoaoMariz/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RS6_OOP_P1_2022_05_JoaoMariz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
